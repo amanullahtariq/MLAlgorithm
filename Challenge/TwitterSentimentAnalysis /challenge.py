2c362131e840b4e9b3533987ab7f4df3b9086481
Amanullah Tariq <amanullahtariq@gmail.com>
2017-01-25 00:05:48 +0100

    updated

---
+++ Challenge/TwitterSentimentAnalysis /challenge.py
@@ -0,0 +1,35 @@
+import tweepy
+from textblob import TextBlob
+#from textblob_fr import PatternTagger, PatternAnalyzer
+import csv
+
+
+#Step 2b - Function of labelisation of analysis
+def get_label(analysis, threshold = 0):
+	if analysis.sentiment[0]>threshold:
+		return 'Positive'
+	else:
+		return 'Negative'
+
+
+consumer_key = "Nw8isvM0lAwA8AxTTNWTSYj6Y"
+consumer_secret = "1AjAhE6KqDGYpw3JXvc0KGdc67LPiPafqA6Ln5ES0egOmPL88B"
+access_token = "1117680990-R9FTQVelroPKjli0237OpWCQNqwdvJ9bhMqxEsU"
+access_token_secret = "mbG4bFbW1zX7UiwX1lSNwgFpaiWqpXLHLWy08jSnkKXj1"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+this_candidate_polarities = []
+public_tweets = api.search('Trump', count=100)
+with open('thefile.csv', 'w') as this_candidate_file:
+    this_candidate_file.write('tweet,sentiment_label\n')
+    for tweet in public_tweets:
+        analysis = TextBlob(tweet.text)
+        print(analysis)
+        print(analysis.sentiment[0])
+        this_candidate_polarities.append(analysis.sentiment[0])
+        this_candidate_file.write('%s,%s\n' % (tweet.text.encode('utf8'), get_label(analysis)))
+
+
