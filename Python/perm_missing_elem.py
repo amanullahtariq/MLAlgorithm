19ea4c4c7e185c33dfbb7124d2e32a13341655be
Amanullah Tariq <amanullahtariq@gmail.com>
2018-05-08 01:12:53 +0500

    quicksort

--- Python/perm_missing_elem.py
+++ Python/perm_missing_elem.py
@@ -1,4 +1,5 @@
-# An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+# An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that
+# exactly one element is missing.
 #
 # Your goal is to find that missing element.
 #
@@ -26,6 +27,10 @@
 # expected worst-case time complexity is O(N);
 # expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
 
+# -----------------------------------------------------------------------#
+#                       Detected time complexity:
+#                       O(N) or O(N * log(N))
+# -----------------------------------------------------------------------#
 
 def solution(A):
     num = 1
