fdb451e609e12b7f56068b08bcffe9dafcc3a796
Amanullah Tariq <amanullahtariq@gmail.com>
2018-10-21 20:41:34 +0200

    booking system deleted

--- BookingSystem/booking_system.sh
+++
@@ -1,9 +0,0 @@
-python -m unittest test_booking_detail
-
-python booking_system.py --input_file_path=input.txt
-
-# Changing order of the meeting
-#python booking_system.py --input_file_path=input1.txt
-
-# Testing if request time is greater than the booking time
-#python booking_system.py --input_file_path=input2.txt
